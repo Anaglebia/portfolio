b23233102831ddefc00cc6d6e4b9af3b2d065fbb
Ana Glebia Bezerra <anabezerra@movida.com.br>
2022-11-21 15:04:02 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MainComponent,
+    HabilidadesComponent,
+    SobreComponent,
+    ProjetosComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
